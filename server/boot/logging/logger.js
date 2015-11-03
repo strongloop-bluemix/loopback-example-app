da9cfc6fb5d4dfb4f558d690ae188b0fbce014c2
Dennis Ashby <dashby@Denniss-MacBook-Pro.local>
2015-11-03 17:06:35 -0600

    Updated to fix issues annoying slc build

--- server/boot/logging/logger.js
+++ server/boot/logging/logger.js
@@ -1,8 +1,17 @@
 module.exports = function (server) {
+  var fs = require('fs');
+
+  //Create logging directory
+  try {
+    fs.mkdirSync(server.get('logger').directory);
+  } catch (e) {
+    console.log(e);
+  }
+
   //Install Winston logging
   server.logger = require('winston');
 
   server.logger.level = server.get('logger').level;
 
-  server.logger.add(server.logger.transports.File, { filename: server.get('logger').filename });
+  server.logger.add(server.logger.transports.File, {filename: server.get('logger').directory + '/' + server.get('logger').filename});
 };
